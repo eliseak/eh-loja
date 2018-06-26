1efade39d7cccff8283beb3a9258daf227813345
Elise <eliseakemi@gmail.com>
2018-06-26 11:50:13 -0300

    Corrigir busca GET por email

--- index.js
+++ index.js
@@ -46,13 +46,19 @@ app.get('/clientes', (req, res) => {
 })
 
 // LER o cliente no banco por EMAIL
-app.get('/clientes/:email', (req, res) => {
+app.get('/cliente/:email', (req, res) => {
     let query = {
         email: req.params.email
     }
 
-    res.send(req.params.email);
+    req.db.collection('clientes').findOne(query, (error, data) => {
+        if (error){
+            res.status(500).send();
+            return;
+        }
 
+        res.send(data);
+    })
 })
 
 // Primeira porta para o Heroku, caso não encontrado: utiliza porta 3000
